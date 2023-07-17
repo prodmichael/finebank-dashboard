700344e4ba02ff408f6b62e695463bb0f6dd334f
Michael <102651644+prodmichael@users.noreply.github.com>
2023-07-17 17:09:08 +0300

    upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './module/auth/pages/login/login.component';
+import { SignupComponent } from './module/auth/pages/signup/signup.component';
+import { ForgotPasswordComponent } from './module/auth/pages/components/forgot-password/forgot-password.component';
+import { PlatformComponent } from './platform/platform.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
+  declarations: [AppComponent, LoginComponent, SignupComponent, ForgotPasswordComponent, PlatformComponent],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
