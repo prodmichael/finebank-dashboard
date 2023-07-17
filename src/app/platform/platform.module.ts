d596a711e7cd6ca95568bee3ae7dcac4a1ed8810
Michael <102651644+prodmichael@users.noreply.github.com>
2023-07-17 21:08:11 +0300

    upd

--- src/app/platform/platform.module.ts
+++ src/app/platform/platform.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedComponent } from './shared/shared.component';
 import { ButtonComponent } from './shared/ui-kit/button/button.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UserPanelComponent } from './components/sidebar/components/user-panel/user-panel.component';
 
 
 
 @NgModule({
   declarations: [
     SharedComponent,
-    ButtonComponent
+    ButtonComponent,
+    SidebarComponent,
+    UserPanelComponent
   ],
   imports: [
     CommonModule
