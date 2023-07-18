d9ff3a4c0714885271e882af046cd39831c309dd
Michael <102651644+prodmichael@users.noreply.github.com>
2023-07-18 17:58:05 +0300

    upd

--- src/app/module/auth/auth.module.ts
+++ src/app/module/auth/auth.module.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AppSpacingModule } from '../shared/ui-kit/spacing/app-spacing.module';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './pages/login/login.component';
@@ -9,6 +9,6 @@ import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule, MatIconModule],
+  imports: [CommonModule, AuthRoutingModule, MatIconModule, AppSpacingModule],
 })
 export class AuthModule {}
--- src/app/module/auth/pages/login/login.component.ts
+++ src/app/module/auth/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-
+  form!: FormGroup;
 }
