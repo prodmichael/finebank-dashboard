f18d97055072415b994e69d4227506def1141b20
Michael <102651644+prodmichael@users.noreply.github.com>
2023-07-20 12:27:45 +0300

    upd

--- src/app/module/shared/ui-kit/checkbox/checkbox.module.ts
+++ src/app/module/shared/ui-kit/checkbox/checkbox.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CheckboxComponent } from './checkbox.component';
 @NgModule({
   declarations: [CheckboxComponent],
   exports: [CheckboxComponent],
-  imports: [CommonModule, MatCheckboxModule],
+  imports: [CommonModule, MatCheckboxModule, FormsModule],
 })
 export class CheckboxModule {}
