b6f88d2458d6e99b74f62d1401bd4c4c620c67f5
Michael <102651644+prodmichael@users.noreply.github.com>
2023-07-26 16:09:44 +0300

    upd

--- src/app/module/platform/pages/overview/components/overview-card/overview-card-balance.component.ts
+++ src/app/module/platform/pages/overview/components/overview-card/overview-card-balance.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-overview-card-balance',
   templateUrl: './overview-card-balance.component.html',
   styleUrls: ['./overview-card-balance.component.scss'],
 })
-export class OverviewCardComponent {
+export class OverviewCardComponent implements OnInit {
   isActive = true;
   currentCardIndex: number = 0;
   cards: any[] = [
@@ -24,23 +24,34 @@ export class OverviewCardComponent {
     {
       img: '',
       type: 'Credit card',
-      number: '**** **** **** 8398',
-      balance: '$5000',
-    },
-    {
-      img: '',
-      type: 'Debit card',
-      number: '**** **** **** 5243',
-      balance: '$12000',
+      number: '**** **** **** 8358',
+      balance: '$53240',
     },
   ];
+  indicators: number[] = Array.from({ length: this.cards.length }, (_, i) => i);
+  currentIndicatorIndex: number = 0;
+  maxVisibleIndicators: number = 3;
+  visibleIndicators: number[] = [];
+
+  ngOnInit() {
+    this.updateVisibleIndicators();
+  }
 
   nextCard() {
     this.currentCardIndex = (this.currentCardIndex + 1) % this.cards.length;
+    this.currentIndicatorIndex = this.currentCardIndex;
   }
 
   previousCard() {
     this.currentCardIndex =
       (this.currentCardIndex - 1 + this.cards.length) % this.cards.length;
+    this.currentIndicatorIndex = this.currentCardIndex;
+  }
+
+  updateVisibleIndicators() {
+    this.visibleIndicators = this.indicators.slice(
+      this.currentCardIndex,
+      this.currentCardIndex + this.maxVisibleIndicators
+    );
   }
 }
