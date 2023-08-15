89ce50a22fff1af8b36096516c11953c81d87ff1
Michael <102651644+prodmichael@users.noreply.github.com>
2023-08-15 14:42:37 +0300

    upd

---
+++ src/app/module/platform/pages/goals/goals.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GoalsHomeComponent } from './pages/goals-home/goals-home.component';
+
+
+
+@NgModule({
+  declarations: [
+    GoalsHomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GoalsModule { }
---
+++ src/app/module/platform/pages/goals/pages/goals-home/goals-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-goals-home',
+  templateUrl: './goals-home.component.html',
+  styleUrls: ['./goals-home.component.scss']
+})
+export class GoalsHomeComponent {
+
+}
